97171e1d3a9bdf5eb78bc690a4b7a48fc66ac5fb
Tevfik Tarık Alim <99840756+tarikalim@users.noreply.github.com>
2024-02-11 15:56:38 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,10 +2,8 @@ import requests
 
 def get_weather(city):
     try:
-        # Kullanıcıdan alınan şehir adını wttr.in servisine gönder
         response = requests.get(f'http://wttr.in/{city}?format=%C+%t')
         
-        # İstek başarılıysa hava durumu bilgilerini al
         if response.status_code == 200:
             weather_data = response.text.strip()
             print(f'Weather info: {weather_data}')
